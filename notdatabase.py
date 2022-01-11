8c011267786503f673a43fe268c60fc88077589f
Arch Huang <ArchKitten05@gmail.com>
2022-01-11 13:57:53 -0800

    notdatabase.py did not get committed so i'm doing it now

---
+++ notdatabase.py
@@ -0,0 +1,9 @@
+class NotDatabase:
+    def __init__(self):
+        print('Hello World!')
+        self.a = 1
+        self.b = 2
+        print(self.a + self.b)
+
+    def palindrome(self):
+        print('Hollow World!')
