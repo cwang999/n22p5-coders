e9949e201651e0a76df77c439e879313f2b12ea7
samayass <samaya2024@gmail.com>
2022-02-04 14:04:38 -0800

    added life and spaceshooters game

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def index():
 def search():
     return render_template("search.html")
 
+@app.route('/spaceshooters/')
+def spaceshooters():
+    return render_template("spaceshooters.html")
+
+
+@app.route('/life/')
+def life():
+    return render_template("life.html")
+
 
 # Space Pages
 @app.route('/learn_planets/')
